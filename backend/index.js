8733cf9e994b8a2ff158e0937f9af0c1f8ec6a67
maki giuseppe de la vega silva <101688628+panteraxo@users.noreply.github.com>
2023-12-17 17:22:36 -0500

    last changes on 17/12/23

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,7 +12,21 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log('Server listening on port 3000');
+});
+
+app.use("/backend/user",userRoutes);
+app.use("/backend/auth",authRoutes);
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
 });
